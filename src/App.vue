1fee0a7bcd1b9266361c24e22b86fa6ebbc1f630
deepak <deepamdeebu@gmail.com>
2024-02-22 11:55:47 +0530

    Blog Website : Created a blog website sing composition api, async api fetch and usage of composibles .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
+</script>
 
 <style>
 #app {
@@ -9,18 +18,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
